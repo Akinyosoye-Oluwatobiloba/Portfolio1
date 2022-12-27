e2932850fc95057dcf4a5ec49dd39ce7fbddfe22
Tech-Kid <96727089+Akinyosoye-Oluwatobiloba@users.noreply.github.com>
2022-12-27 21:20:04 +0100

    portfolio

---
+++ pages/resume.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import Head from 'next/head';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+const resume = () => {
+  return (
+    <div>
+      <Head>
+        <title>Clint | Resume</title>
+        <meta
+          name='description'
+          content='I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences.'
+        />
+        <link rel='icon' href='/fav.png' />
+      </Head>
+
+      <div className='max-w-[940px] mx-auto p-2 pt-[120px]'>
+        <h2 className='text-center'>Resume</h2>
+        <div className='bg-[#d0d4d6] my-4 p-4 w-full flex justify-between items-center'>
+          <h2 className='text-center'>Oluwatobiloba Akinyosoye</h2>
+          <div className='flex'>
+            <a
+              href='https://www.linkedin.com/in/oluwatobiloba-akinyosoye/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FaLinkedinIn size={20} style={{ marginRight: '1rem' }} />
+            </a>
+            <a
+              href='https://github.com/Akinyosoye-Oluwatobiloba'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FaGithub size={20} style={{ marginRight: '1rem' }} />
+            </a>
+          </div>
+        </div>
+        <div className='text-center py-4 text-xl font-bold uppercase tracking-wider'>
+          <div className='hidden sm:block'>
+            <p>
+              Proven Leadership <span className='px-1'>|</span> Backend Development{' '}
+              <span className='px-1'>|</span> Problem Solving Abilities
+            </p>
+          </div>
+          <div className='block sm:hidden'>
+            <p>Proven Leadership</p>
+            <p className='py-2'>Web Development</p>
+            <p>Complex Problem Solving</p>
+          </div>
+        </div>
+        <p>
+          Analytical, innovative, and motivated web development professional
+          with experience in customer service, team leadership, and
+          organizational effectiveness in fast-paced and challenging
+          environments. Adept at developing strategies and driving streamlined
+          operations. Diverse analytical skills, team collaboration, and
+          relationship building. Consummate professional, and motivated leader,
+          with solid interpersonal abilities and complex problem-solving skills.
+          Effective and proven track record of critical thinking, idea
+          generation, and optimizing efficiencies.
+        </p>
+
+        {/* Skills */}
+        <div className='text-center py-4'>
+          <h5 className='text-center underline text-[18px] py-2'>Skills</h5>
+          <p className='py-2'>
+            <span className='font-bold'>Technical Skills</span>
+            <span className='px-2'>|</span>Front-End Web Developer
+            <span className='px-2'>|</span> HTML
+            <span className='px-2'>|</span>CSS
+            <span className='px-2'>|</span>Javascript
+            <span className='px-2'>|</span>React
+            <span className='px-2'>|</span>Next JS
+            <span className='px-2'>|</span>SQL
+            <span className='px-2'>|</span>PHP
+            <span className='px-2'>|</span>Laravel
+            <span className='px-2'>|</span>Tailwind
+            <span className='px-2'>|</span> Solidity
+            <span className='px-2'>|</span> RESTAPI
+          </p>
+          
+        </div>
+
+        <h5 className='text-center underline text-[18px] py-4'>
+          Professional Experience
+        </h5>
+        {/* Experience */}
+        <div className='py-4'>
+          <p className='italic'>
+            <span className='font-bold italic'>
+            YOUR COMPANY
+            </span>
+            <span className='px-2'>|</span>YOUR COMPANY ADDRESS
+          </p>
+          <p className='py-1 italic'>Backend End Web Developer</p>
+          <ul className='list-disc list-outside px-7 py-1 leading-relaxed'>
+            <li>
+              Looking Forward to your companies tasks
+            </li>
+           
+          </ul>
+        </div>
+
+</div>
+    </div>
+  
+        
+          
+  
+  );
+};
+
+export default resume;
