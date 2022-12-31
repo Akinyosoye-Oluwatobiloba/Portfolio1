4421c2ede5ffe659f39357145bb5930b1c568247
Tech-Kid <96727089+Akinyosoye-Oluwatobiloba@users.noreply.github.com>
2022-12-31 18:59:49 +0100

    modified

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Main from '../components/Main'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Godwin | Front-End Developer</title>
+        <meta name="description" content="I’m a Back-end web developer specializing in building exceptional digital experiences and interactions." />
+        <link rel="icon" href="/fav.png" />
+      </Head>
+    <Main />
+    <About />
+    <Skills />
+    <Projects />
+    <Contact />
+    </div>
+  )
+}
